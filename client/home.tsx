ab6901eae4c2b488d2488e949f31dcae4fab8558
SaphireVert <jerome.cosandey@gmail.com>
2023-04-26 15:29:23 +0200

    feat: client OIDC login to the server

--- client/home.tsx
+++ client/home.tsx
@@ -3,13 +3,15 @@ import * as React from 'react'
 import { Base } from '@epfl/epfl-sti-react-library'
 import { OIDCContext, StateEnum, LoginButton, IfOIDCState, LoggedInUser } from '@epfl-si/react-appauth'
 import Links from './components/Links'
+import { Accounts } from 'meteor/accounts-base'
 
 export default function Home() {
   return (
     <Base asideMenuItems={[]}>
       <OIDCContext authServerUrl = { 'http://localhost:8080/realms/react-starter-kit/' }
                       client = { { clientId: "react-starter-kit",
-                                   redirectUri: "http://localhost:3000/" } }>
+                                   redirectUri: "http://localhost:3000/" } }
+                                   onNewToken={ ( token ) => oidcLogin(token) }>
       <LoginButton inProgressLabel={ <>⏳</> }/>
       <IfOIDCState is={ StateEnum.LoggedIn }>
         <p>Hello, <LoggedInUser field="preferred_username" />!</p>
@@ -18,4 +20,21 @@ export default function Home() {
     </OIDCContext>
     </Base>
   )
+}
+
+const oidcLogin = (token: string) => {
+  Accounts.callLoginMethod({
+  methodArguments: [
+    {
+      oidcToken: token,
+    },
+  ],
+  userCallback: serverSideError => {
+      if (serverSideError) {
+        // Only happens when the OIDC login has succeeded but then the Meteor server fails to validate it somehow.
+        const { error, reason, details, message } = serverSideError
+        alert(`Error ${error} on the server\n\nreason: ${reason}\ndetails: ${details}\n message: ${details}\n`)
+      }
+    },
+  });
 }
