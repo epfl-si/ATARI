1090230433319f16f4df72a98b43e007e0923a9e
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-05-01 18:42:51 +0200

    [refactor] Call `this.added` explicitly
    This lets us compute fields that we want to disclose to the client (instead of being restricted to filtering MongoDB columns)

--- server/OIDC/OIDC.ts
+++ server/OIDC/OIDC.ts
@@ -22,14 +22,8 @@ async function checkTokenAndUpsertUser (options: Options) : Promise<LoginStatus>
 // The Meteor server doesn't really need the personal identifying data. The cached copy it has can become stale.
 Meteor.publish(null, function () {
     if (! this.userId) return
-    return Meteor.users.find({
-      _id: this.userId
-    }, {
-      fields: {
-        given_name: 1,
-        family_name: 1
-      }
-    });
+  const [me] = Meteor.users.find({_id: this.userId}).fetch()
+  this.added('users', me._id, {given_name: me.given_name, family_name: me.family_name});
   })
 
 
