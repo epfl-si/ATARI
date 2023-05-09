8d79cc6d3794daa1c8075d20b788cee9ae8ef045
SaphireVert <jerome.cosandey@gmail.com>
2023-05-09 10:48:53 +0200

    chore: remove useless function

--- client/components/Virtualize.tsx
+++ client/components/Virtualize.tsx
@@ -117,18 +117,6 @@ const ListboxComponent = React.forwardRef<
   );
 });
 
-function random(length: number) {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-
-  for (let i = 0; i < length; i += 1) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-
-  return result;
-}
-
 const StyledPopper = styled(Popper)({
   [`& .${autocompleteClasses.listbox}`]: {
     boxSizing: 'border-box',
