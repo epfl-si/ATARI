7fb97b652b61aca4bb86736c87bf449bffc2d3f2
SaphireVert <jerome.cosandey@gmail.com>
2023-05-23 11:15:20 +0200

    refactor: wrap callback and queryParams into functions

--- server/cadidb/userDetails.ts
+++ server/cadidb/userDetails.ts
@@ -7,16 +7,22 @@ export default [
     },
     function fetchPersonSql(added, sciper){
         let connection = mysql.createConnection(settings.mysql);
-        connection.query({
-            sql: 'SELECT * from Personnes where sciper = ?;',
-            timeout: 5000,
-            values: [sciper]
-        }, (error, results, fields) => { 
-            console.log("Person:", results)
-            results.map((result) => {
-                console.log("added:", sciper, result);
-                added("userDetails", sciper , result);
-            })
-        })
+        connection.query(queryParams(sciper), wrapResultMap(added, sciper))
     }
 ]
+
+const queryParams = (sciper) => {
+    return {
+        sql: 'SELECT * from Personnes where sciper = ?;',
+        timeout: 5000,
+        values: [sciper]
+    }
+}
+
+function wrapResultMap(added, sciper){
+    return (error, results, fields) => { 
+        results.map((result) => {
+            added("userDetails", sciper , result);
+        })
+    }
+}
