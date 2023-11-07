97524d1145f5a0cd87190dfe2f23b696574af450
Azecko <samikarin24@gmail.com>
2023-11-07 11:25:52 +0100

    [feature] If no profile picture loaded, put a "placeholder"
    If the user doesn't have a picture on people.epfl.ch, it will put a "placholder" one with base64 img.

--- client/components/UserDetails.tsx
+++ client/components/UserDetails.tsx
@@ -102,7 +102,10 @@ function UserDetails(props:{user:DigestUser}) {
                 <img
                   className="img-fluid"
                   src={`https://people.epfl.ch/private/common/photos/links/${props.user.sciper}.jpg`}
-                  alt="Shawna Reeves O'Neill Edwards"
+                  onError={({ currentTarget }) => {
+                    currentTarget.onerror = null;
+                    currentTarget.src = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTYgMjU2Ij48cGF0aCBkPSJNMTAyLjQzIDEyNC41M1MxMDUuNSAxNTIgMTI4IDE1MnMyNS41Ny0yNy40NiAyNS41Ny0yNy40NiA0Ljg1IDEgNy4xOS05LjI4YzEuNS02LjctLjUtOC40OS0xLjc2LTguNDloLTEuMkMxNjMuNjggNzQuMjggMTQwIDY0IDEyOCA2NHMtMzUuNjggMTAuMjgtMjkuOCA0Mi43Nkg5N2MtMS4yNiAwLTMuMjYgMS43OS0xLjc2IDguNDkgMi4zNCAxMC4zMyA3LjE5IDkuMjggNy4xOSA5LjI4ek0xNzAuMzYgMTY0Yy0yMC4yNi0zLjg5LTI0LjM0LTgtMjUuMS0xMS42NGEyOS4xNSAyOS4xNSAwIDAgMS0zNC41MiAwYy0uNzQgMy42NC00Ljg0IDcuNzItMjUuMSAxMS42NC0yMC44NSA0LTIwLjU3IDIxLjE4LTIwLjU3IDI0aDEyNS44NmMwLTIuODUuMjgtMjAtMjAuNTctMjR6Ii8+PC9zdmc+Cg=='
+                  }}
                 />
               </div>
             </div>
