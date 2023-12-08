f7e925d295bf1e40a01678afc20c07af614d3d9e
Azecko <samikarin24@gmail.com>
2023-12-08 16:08:15 +0100

    [fix] Missing people and duplicate people
    Some people weren't findable in the search bar and some were duplicated. It's now fixed by doing a `SELECT DISTINCT` instead of a simple `SELECT`.

--- server/cadidb/digestUsersQueries.ts
+++ server/cadidb/digestUsersQueries.ts
@@ -8,9 +8,10 @@ async function insertDigestUser({ first_name, last_name, phone_number, sciper, g
 // Todo: cache these informations in order to not surcharge the db
 export async function getDigestList(added){
     return await cadiSqlQuery({
-        sql: `SELECT p.nom as last_name, p.prenom as first_name, p.sciper, p.email, p.username as gaspar, b.telephone1 as phone_number 
+        sql: `SELECT DISTINCT p.nom as last_name, p.prenom as first_name, p.sciper, p.email, p.username as gaspar, b.telephone1 as phone_number 
                 FROM Personnes p
-                LEFT JOIN Bottin b ON p.sciper = b.sciper  where p.username IS NOT NULL limit 30000`,
+                LEFT JOIN Bottin b ON p.sciper = b.sciper
+                WHERE p.username IS NOT NULL`,
         timeout: 50000,
     }, (error, results, fields) => { 
         if(results[0]){
