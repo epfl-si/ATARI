025c7240ff132cc184d96ada06e48cae9e0da3ed
Azecko <30987143+Azecko@users.noreply.github.com>
2024-04-12 11:42:30 +0200

    [refactor] Complete `/checkLDAP` (#36)
    The checkLDAP route is now a 1:1 from idp-exop

--- client/components/CheckLDAP.tsx
+++ client/components/CheckLDAP.tsx
@@ -8,6 +8,8 @@ import { useParams } from "react-router-dom";
 function CheckLDAP() {
   const { sciper } = useParams();
   const [user, setUser] = useState({});
+  const [scoLDAP, setScoLDAP] = useState({});
+  const [homeDir, setHomeDir] = useState({});
 
   useEffect(() => {
     Meteor.call('checkLDAP.user', sciper, function(err, res) {
@@ -17,12 +19,31 @@ function CheckLDAP() {
         setUser(res[0]);
       }
     })
+    Meteor.call('scoLDAP.user', sciper, function(err, res) {
+      if(err) {
+        console.log(err)
+      } else {
+        setScoLDAP(res[0]);
+      }
+    })
   }, [])
 
+  useEffect(() => {
+    if(user.uid !== undefined) {
+      Meteor.call('homedir.user', user.uid[0], function(err, res) {
+        if(err) {
+          console.log(err)
+        } else {
+          setHomeDir(res[0]);
+        }
+      })
+    }
+  }, [user])
 
   return (
     <Container>
-      <h1>LDAP</h1>
+      <h1>CheckLDAP</h1>
+      <h2>Info from ldap.epfl.ch</h2>
       <ul>
         {
             Object.entries(user).map(([key,val]) => (
@@ -35,6 +56,40 @@ function CheckLDAP() {
                 </li>
               ) : (
                 <li key={key}><strong>{key}</strong>: {user[key]}</li>
+              )
+            ))
+        }
+      </ul>
+      <h2>User homedir info</h2>
+      <ul>
+        {
+            Object.entries(homeDir).map(([key,val]) => (
+              typeof(homeDir[key]) == 'object' ? (
+                <li key={key}>
+                  <strong>{key}</strong>
+                  <ul>
+                    {homeDir[key].map(element => <li key={element}>{element}</li>)}
+                  </ul>
+                </li>
+              ) : (
+                <li key={key}><strong>{key}</strong>: {homeDir[key]}</li>
+              )
+            ))
+        }
+      </ul>
+      <h2>Info from scoldap.epfl.ch</h2>
+      <ul>
+        {
+            Object.entries(scoLDAP).map(([key,val]) => (
+              typeof(scoLDAP[key]) == 'object' ? (
+                <li key={key}>
+                  <strong>{key}</strong>
+                  <ul>
+                    {scoLDAP[key].map(element => <li key={element}>{element}</li>)}
+                  </ul>
+                </li>
+              ) : (
+                <li key={key}><strong>{key}</strong>: {scoLDAP[key]}</li>
               )
             ))
         }
