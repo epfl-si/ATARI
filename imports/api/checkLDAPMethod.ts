d92aa5ed78493c25c21e1480684f0ac7b3113099
Azecko <samikarin24@gmail.com>
2024-04-12 11:41:17 +0200

    [refactor] Complete `/checkLDAP`
    The checkLDAP route is now a 1:1 from idp-exop

--- imports/api/checkLDAPMethod.ts
+++ imports/api/checkLDAPMethod.ts
@@ -19,5 +19,39 @@ Meteor.methods({
         await client.unbind();
 
         return searchEntries;
+    },
+    'scoLDAP.user': async function(sciper) {
+        const url = 'ldap://scoldap.epfl.ch';
+        const searchDN = 'o=epfl,c=ch';
+        
+        const client = new Client({
+            url
+        });
+
+        const { searchEntries, searchReferences } = await client.search(searchDN, {
+            scope: 'sub',
+            filter: `(&(objectclass=EPFLorganizationalPerson)(uniqueIdentifier=${sciper}))`,
+        });
+
+        await client.unbind();
+
+        return searchEntries;
+    },
+    'homedir.user': async function(username) {
+        const url = 'ldap://ldap.epfl.ch';
+        const searchDN = 'ou=automaps,o=epfl,c=ch';
+        
+        const client = new Client({
+            url
+        });
+
+        const { searchEntries, searchReferences } = await client.search(searchDN, {
+            scope: 'sub',
+            filter: `(cn=${username})`,
+        });
+
+        await client.unbind();
+
+        return searchEntries;
     }
 })
