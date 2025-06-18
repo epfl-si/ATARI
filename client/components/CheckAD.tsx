35c70ed54e2e9a2c131cf34563529e955b5a3f94
Loic Piguet <loic.piguet@epfl.ch>
2025-06-18 14:32:43 +0200

    [fix] change the attribute checked for the existence of an Exchange Mailbox

--- client/components/CheckAD.tsx
+++ client/components/CheckAD.tsx
@@ -69,7 +69,7 @@ function CheckAD() {
           </ul>
         </span>
         <span><strong>Expire</strong> : {user.accountExpires == 9223372036854775807 ? 'Jamais' : new Date(((user.accountExpires / 10000000) - 11644473600) * 1000).toLocaleDateString('en-US')}</span>
-        <span><strong>Exchange mailbox</strong> : {user.msExchMailboxGuid ? "Oui" : "Non"}</span>
+        <span><strong>Exchange mailbox</strong> : {user.msExchRecipientTypeDetails ? "Oui" : "Non"}</span>
         <span><strong>Where is the mailbox ?</strong> : {user.msExchRecipientTypeDetails == "1" ? "Exchange OnPrem" : user.msExchRecipientTypeDetails == "2147483648" ? "Exchange Online" : "N/A"}</span>
         <span><strong>A le droit Distrilog</strong> : {distrilog.length ? 'Oui' : 'Non'}</span>
 
