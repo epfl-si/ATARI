31e666d43317974994ab2acaf078d34c2dae67af
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-11-07 16:45:52 +0100

    [undepends] `@epfl-si/react-appauth`

--- imports/types/UserInfo.ts
+++ imports/types/UserInfo.ts
@@ -3,8 +3,8 @@ declare module "meteor/meteor" {
 		interface User {
 			given_name: String
 			family_name: String
-			groups: String[]   // Server only
-			has_access_right: boolean  // Client only
+            groups: String[]   // Server only
+            has_access_right: boolean  // Client only
 		}
 	}
 }
